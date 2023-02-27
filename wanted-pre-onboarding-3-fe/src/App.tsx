5bfef5c2027f3b2f45f94b07c379983214bc81af
HJeong1200 <hjeong1200@gmail.com>
2023-02-27 12:39:05 +0900

    feat: Main 및 Sidebar 구현 및 기본 CSS 설정

--- wanted-pre-onboarding-3-fe/src/App.tsx
+++ wanted-pre-onboarding-3-fe/src/App.tsx
@@ -1,8 +1,18 @@
 import "App.css";
 import Header from "components/Header";
+import Main from "components/Main";
+import Sidebar from "components/Sidebar";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="contents">
+        <Sidebar />
+        <Main />
+      </div>
+    </>
+  );
 }
 
 export default App;
